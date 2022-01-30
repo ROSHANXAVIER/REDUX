09f4416b33886fe150cc0b4b3462f1d0ff6f8dd1
Roshan <rxgody@gmail.com>
2022-01-30 15:33:00 +0530

    REDUX

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useSelector,useDispatch } from "react-redux";
+import {increment,decrement} from "./action"
+const App = () => {
+  const counter=useSelector(state=>state.counter)
+  const dispatch=useDispatch()
+  return (
+    <div>
+      <h3>CrowdOverflow</h3>
+      <h2>Counter : {counter} </h2>
+      <button onClick={()=>dispatch(increment())}>+</button>
+      <button onClick={()=>dispatch(decrement())}>-</button>
+    </div>
+  );
+};
+
+export default App;
